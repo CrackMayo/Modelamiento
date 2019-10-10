0692a575177d3e0dbb81a2eae0711d7f4c425d4b
CrackMayo <tamayo998@hotmail.com>
2019-10-10 09:50:02 -0500

    Paratrooper solution added

--- Paracaidista.py
+++ Paracaidista.py
@@ -1,53 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 26 12:10:43 2018
-
-@author: carlos
-"""
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-plt.xlim([-15,15])
-plt.ylim([0,15])
-xo = 10
-vo = 0
+y = 17.5
+m = 0.1
 g = 9.8
-t = 0
-k = 100
-ka = 2
-u = 0
-m = 30
-lo = 2
-x = xo
-v = vo
 dt = 0.01
-carro, = plt.plot(np.array([x,x+1,x+1,x,x]),np.array([1,1,2,2,1]))
-llanta1, = plt.plot(x,1,'o')
-llanta2, = plt.plot(x+1,1,'o')
-resorte, = plt.plot(np.array([0,x]),np.array([1.5,1.5]))
-ts = []
-ys = []
-while(t<40):
-    t1 = t + dt
-    v1 = ((k*(lo-x)-(ka*v))/m) * (t1-t) + v
-    x1 = (v1)*(t1-t) + x
-    x = x1
-    t = t1
-    v = v1        
-    t = t + 0.1
-    carro.set_xdata(np.array([x,x+1,x+1,x,x]))
-    llanta1.set_xdata(x)
-    llanta2.set_xdata(x+1)
-    if(x<0):
-        resorte.set_xdata(np.array([0,x+1]))
-    else:
-        resorte.set_xdata(np.array([0,x]))
-    plt.pause(0.1)
-    ts = np.concatenate((ts,np.array(x)),axis=None)
-    ys = np.concatenate((ys,np.array(t)),axis=None)
-    print("x = ",x)
-plt.figure(2)
-plt.plot(ys,ts,'b')    
-plt.title("posicion vs tiempo")
-plt.xlabel("tiempo")
-plt.ylabel("posicion")
+v = 0
+r = 0.3
+a = np.pi*(r**2)
+t=0
+k=0.98*0.8*a
+plt.xlim(0,40)
+plt.ylim(0,25)
+masa,=plt.plot(20,y,'o')
+paracaidas, = plt.plot(np.array([20,17,18,19,20,21,22,23,20,23,20,17]),np.array([y,y+1.5,y+2,y+2.25,y+2.4,y+2.25,y+2,y+1.5,y,y+1.5,y+1.5,y+1.5]))
+edificio,=plt.plot(np.array([5,5,15,15,5,5,5,15,15,5,5,5,15,15,5,5,5,15,15,5,5,5,15,15,5,5,5,15,15]), np.array([0,3.5,3.5,0,0,3.5,7,7,3.5,3.5,7,10.5,10.5,7,7,10.5,14,14,10.5,10.5,14,17.5,17.5,14,14,17.5,21,21,17.5]),'k')
+Vt = np.array([],float)
+Vy = np.array([],float)
+Vv = np.array([],float)
+while y > 0.03:
+    plt.pause(dt)
+    vk=(-g+(k*v**2/m))*dt+v
+    yk=v*dt+y
+    y=yk
+    v=vk
+    paracaidas.set_ydata(np.array([y,y+1.5,y+2,y+2.25,y+2.4,y+2.25,y+2,y+1.5,y,y+1.5,y+1.5,y+1.5]))
+    masa.set_ydata(yk)
+    t=t+dt
+    Vv= np.append(Vv,-vk)
+    Vy= np.append(Vy,yk)
+    Vt= np.append(Vt,t)  
+    print("%1.2f"%t, yk) 
+    
+    
+plt.figure("Tiempo vs Altura")  
+plt.xlabel("Tiempo (s)", fontsize = 10)
+plt.ylabel("Altura (m)", fontsize = 10)  
+plt.plot(Vt,Vy,'b') 
+plt.figure("Tiempo vs Velocidad")  
+plt.xlabel("Tiempo (s)", fontsize = 10)
+plt.ylabel("Velocidad (m/s)", fontsize = 10)
+plt.plot(Vt,Vv,'b')
